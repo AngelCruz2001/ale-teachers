59567b938d515752d364a4f704127faebf6bde96
Ángel Cruz García <angi_ed01@hotmail.com>
2022-08-14 07:22:43 -0500

    redirect to auth in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Router from 'next/router';
 import styles from 'styles/Home.module.scss'
 
 const Home: NextPage = () => {
+  const router = Router;
+  router.push("/auth");
   return (
     <div className={styles.container}>
 
