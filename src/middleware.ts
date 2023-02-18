5e608f3695f88afc8cc87f32b61b2d7a439ca2b0
AERM2018 <retana.martinez.angel.eduardo@gmail.com>
2023-02-18 00:04:40 -0600

    Fix: Remove fucking middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,24 +19,24 @@ import { NextResponse } from "next/server";
 //   return NextResponse.next();
 // }
 export async function middleware(req: any) {
-  if (req.nextUrl.pathname === "/auth") {
-    const session = await getToken({
-      req: req,
-      secret: process.env.NEXTAUTH_SECRET,
-    });
-    const url = req.nextUrl.clone();
-    url.pathname = "/home";
-    if (session) return NextResponse.rewrite(url);
-  }
-  if (["/home", "/course"].includes(req.nextUrl.pathname)) {
-    const session = await getToken({
-      req: req,
-      secret: process.env.NEXTAUTH_SECRET,
-    });
-    const url = req.nextUrl.clone();
-    url.pathname = "/auth";
-    if (!session) return NextResponse.rewrite(url);
-  }
+  // if (req.nextUrl.pathname === "/auth") {
+  //   const session = await getToken({
+  //     req: req,
+  //     secret: process.env.NEXTAUTH_SECRET,
+  //   });
+  //   const url = req.nextUrl.clone();
+  //   url.pathname = "/home";
+  //   if (session) return NextResponse.rewrite(url);
+  // }
+  // if (["/home", "/course"].includes(req.nextUrl.pathname)) {
+  //   const session = await getToken({
+  //     req: req,
+  //     secret: process.env.NEXTAUTH_SECRET,
+  //   });
+  //   const url = req.nextUrl.clone();
+  //   url.pathname = "/auth";
+  //   if (!session) return NextResponse.rewrite(url);
+  // }
 
   return NextResponse.next();
 }
